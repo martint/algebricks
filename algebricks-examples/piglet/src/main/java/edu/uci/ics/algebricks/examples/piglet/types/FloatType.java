3e3e78f73acecf4d5138e18d8b34d7f9a66fc7d8
vinayakb <vinayakb@34248759-47e7-d47c-fb29-7a6b1deae063>
2011-07-20 05:24:38 +0000

    Minor type changes
    git-svn-id: http://algebricks.googlecode.com/svn/trunk/algebricks@38 34248759-47e7-d47c-fb29-7a6b1deae063

--- algebricks-examples/piglet/src/main/java/edu/uci/ics/algebricks/examples/piglet/types/FloatType.java
+++ algebricks-examples/piglet/src/main/java/edu/uci/ics/algebricks/examples/piglet/types/FloatType.java
@@ -1,6 +1,11 @@
 package edu.uci.ics.algebricks.examples.piglet.types;
 
 public class FloatType extends Type {
+    public static final Type INSTANCE = new FloatType();
+
+    private FloatType() {
+    }
+
     @Override
     public Tag getTag() {
         return Tag.FLOAT;
--- algebricks-examples/piglet/src/main/java/edu/uci/ics/algebricks/examples/piglet/types/LongType.java
+++ algebricks-examples/piglet/src/main/java/edu/uci/ics/algebricks/examples/piglet/types/LongType.java
@@ -1,6 +1,11 @@
 package edu.uci.ics.algebricks.examples.piglet.types;
 
 public class LongType extends Type {
+    public static final Type INSTANCE = new LongType();
+
+    private LongType() {
+    }
+
     @Override
     public Tag getTag() {
         return Tag.LONG;
